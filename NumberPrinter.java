d3ae721a2a40dc7bb45c5b64422c772e07782690
24BCAA-JAYAPRABHA <jayaprabhaelangovan@gmail.com>
2024-09-21 14:34:44 +0530

    Create NumberPrinter.java

---
+++ NumberPrinter.java
@@ -0,0 +1,16 @@
+public class NumberPrinter
+{
+public static void main(String[]args)
+{
+int start=1000;
+int end=2000;
+int numberPerLine=5;
+{
+System.out.print(i + " ");
+if((i - START + 1) % NumbersPerLine == 0)
+{
+System.out.println();
+}
+}
+}
+}
